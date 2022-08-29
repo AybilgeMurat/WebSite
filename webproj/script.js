2c5c35bccd72115d0de277224bd0cd81f9e65066
Ayşe Aybilge MURAT <58533563+AybilgeMurat@users.noreply.github.com>
2022-08-29 16:52:34 +0300

    Add files via upload

---
+++ webproj/script.js
@@ -0,0 +1,37 @@
+﻿var uygulama=angular.module('uygulama',[]);
+
+uygulama.controller('kontrol',function($scope,$http){
+
+$scope.kullanici={};
+
+$scope.hataKullaniciAdi=' ';
+
+$scope.hataKullaniciSifre=' ';
+
+$scope.mesaj=' ';
+
+$scope.formGonder=function(){
+
+$http({
+method:'POST';
+
+url:'uye-giris-form-dogrulama.php',
+
+data:$scope.kullanici,
+
+headers:{'Content-Type':'application/x-www-form-urlencoded'}
+
+})
+success(function(data){
+
+$scope.hataKullaniciAdi=data.kullaniciAdi;
+
+$scope.hataKullaniciAdi=data.kullaniciSifre;
+
+$scope.mesaj=data.mesaj;
+
+});
+
+};
+
+});
---
+++ webproj/uye-giris-form-dogrulama.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+$data=array();
+
+$_POST=json_decode(file_get_contents('php://input'),true);
+
+if(empty($_POST['kullaniciAdi'])){
+
+$data['kullaniciAdi']='Kullanıcı adı gerekli';
+
+}
+
+if(empty($_POST['kullaniciSifre'])){
+
+$data['kullaniciSifre']='Şifre gerekli';
+
+}
+
+if(!empty($data)){
+$data['mesaj']='';
+
+}
+else{
+
+$data['mesaj']='Giriş yapıldı...';
+
+$data['kullaniciAdi']='';
+
+$data['kullaniciSifre']='';
+
+}
+echo json_encode($data);
+
+?>
